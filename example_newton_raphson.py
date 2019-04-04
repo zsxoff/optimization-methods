ae7d96fb1be6e1abb19f27fe43cbdd29b5c54df8
zsxoff <zsxoff@gmail.com>
2019-04-04 20:18:08 +0400

    add newton-raphson method

---
+++ example_newton_raphson.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from simethods.newton_raphson_2d import newton_raphson_2d
+from misc import inputf, func2d_to_string
+from plot_2d import plot_2d
+
+
+def f(x: float, y: float) -> float:
+    """
+    Test function.
+
+    """
+    return y ** 4 + x * y ** 3 + 2 * (y ** 2) * (x ** 2) + y + x ** 4 - x
+
+
+def main() -> None:
+    """
+    Newton-Raphson method example.
+
+    """
+    start_x = inputf('Enter start X: ')
+    start_y = inputf('Enter start Y: ')
+    eps = inputf('eps: ')
+
+    x_min, y_min = newton_raphson_2d(f, start_x, start_y, eps)
+    f_min = f(x_min, y_min)
+
+    f_print = func2d_to_string(f)
+
+    print(f'f               = {f_print}\n'
+          f'x_min           = {x_min}\n'
+          f'y_min           = {y_min}\n'
+          f'f(x_min, y_min) = {f_min}\n')
+
+    plot_2d(f, x_min, y_min)
+
+
+if __name__ == '__main__':
+    main()
---
+++ simethods/newton_raphson_2d.py
@@ -0,0 +1,76 @@
+import numpy as np
+from sympy import Symbol, lambdify
+from typing import Callable, Tuple
+from simethods.borders import find_min_border
+from simethods.bisect import bisect_extremum
+from simethods.argmin import compute_optimal_step
+
+
+def newton_raphson_2d(f: Callable,
+                      x0: float,
+                      y0: float,
+                      eps: float,
+                      max_iters: int = 100,
+                      recount_step_iter=3) -> Tuple[float, float]:
+    """
+    Newton-Raphson method for f(x, y).
+
+    """
+
+    # Use SymPy function.
+    x = Symbol('x')
+    y = Symbol('y')
+    _f = f(x, y)
+
+    # Init initial X, dim 2x1.
+    X = np.zeros((2, 1), dtype=np.float)
+    X[0] = x0
+    X[1] = y0
+
+    # Compute differential equations.
+    _f_dx = _f.diff(x, 1)
+    _f_dy = _f.diff(y, 1)
+    _f_dx_dx = _f_dx.diff(x, 1)
+    _f_dx_dy = _f_dx.diff(y, 1)
+    _f_dy_dx = _f_dy.diff(x, 1)
+    _f_dy_dy = _f_dy.diff(y, 1)
+
+    iterations_count = -1
+    optimal_step = 1
+
+    while True:
+        iterations_count += 1
+        if iterations_count > max_iters:
+            return float(X[0][0]), float(X[1][0])
+
+        # Compute gradient, dim 2x1.
+        grad = np.zeros((2, 1), dtype=np.float)
+        grad[0] = _f_dx.subs([(x, X[0]), (y, X[1])])
+        grad[1] = _f_dy.subs([(x, X[0]), (y, X[1])])
+
+        # Compute H matrix, dim 2x2.
+        H = np.zeros((2, 2), dtype=np.float)
+        H[0, 0] = _f_dx_dx.subs([(x, X[0]), (y, X[1])])
+        H[0, 1] = _f_dx_dy.subs([(x, X[0]), (y, X[1])])
+        H[1, 0] = _f_dy_dx.subs([(x, X[0]), (y, X[1])])
+        H[1, 1] = _f_dy_dy.subs([(x, X[0]), (y, X[1])])
+
+        # Compute optimal step.
+        if iterations_count % recount_step_iter == 0:
+            param = Symbol('z')
+            param_vector = X - param * np.dot(np.linalg.inv(H), grad)
+
+            optimal_step = compute_optimal_step(
+                func=f,
+                parameterized_vector=param_vector,
+                parameter=param,
+                eps=eps)
+
+        # Compute new X, dim 2x1 - 2x2 * 2x1.
+        X_NEW = X - optimal_step * np.dot(np.linalg.inv(H), grad)
+
+        # If norm(X_NEW - X, 2) < eps, return x_min and y_min.
+        if np.linalg.norm(X_NEW - X) < eps:
+            return float(X[0][0]), float(X[1][0])
+
+        X = X_NEW
