c2cd99bcceff20b367432cc7b85c78a496984a3b
zsxoff <zsxoff@gmail.com>
2019-04-04 20:30:40 +0400

    replace part of code with argmin function

--- simethods/conjugate_gradient_2d.py
+++ simethods/conjugate_gradient_2d.py
@@ -1,9 +1,8 @@
 import numpy as np
-from sympy import Symbol, lambdify
+from sympy import Symbol
 from numpy.linalg import norm
-from simethods.bisect import bisect_extremum
-from simethods.borders import find_min_border
 from typing import Callable, Tuple
+from simethods.argmin import compute_optimal_step
 
 
 def conjugate_gradient_2d(f: Callable,
@@ -38,28 +37,21 @@ def conjugate_gradient_2d(f: Callable,
     iterations_count = -1
 
     while True:
-        iterations_count += 0
+        iterations_count += 1
         if iterations_count > max_iters:
             return float(X_0[0][0]), float(X_0[1][0])
 
-        _lambda_vector = X_0 + Symbol('z') * S_0
+        param = Symbol('z')
+        param_vector = X_0 + Symbol('z') * S_0
 
-        _function_z = lambdify(
-            args=(Symbol('z'),),
-            expr=f(_lambda_vector[0][0], _lambda_vector[1][0]))
-
-        _left, _right = find_min_border(
-            f=_function_z,
-            start=0.0,
-            init_step=0.25,
-            step_expand=1.1,
-            max_iters=10)
-
-        argmin_f = bisect_extremum(
-            f=_function_z, a=_left, b=_right, eps=eps, mode='min')
+        optimal_step = compute_optimal_step(
+            func=f,
+            parameterized_vector=param_vector,
+            parameter=param,
+            eps=eps)
 
         # Compute new X, dim 2x1.
-        X_1 = X_0 + argmin_f * S_0
+        X_1 = X_0 + optimal_step * S_0
 
         # Compute new gradient f(X_1), dim 2x1.
         grad_1 = np.zeros((2, 1), dtype=np.float)
