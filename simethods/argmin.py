68ed6483f355dfe94e9b0dbc86bedfddb136f1bb
zsxoff <zsxoff@gmail.com>
2019-04-04 21:04:07 +0400

    typing

--- simethods/argmin.py
+++ simethods/argmin.py
@@ -1,19 +1,19 @@
 from sympy import lambdify
 from simethods.borders import find_min_border
 from simethods.bisect import bisect_extremum
+from numpy import ndarray
+from typing import Callable
+from sympy.core.symbol import Symbol
 
 
-def compute_optimal_step(func, parameterized_vector, parameter, eps):
+def compute_optimal_step(func: Callable, parameterized_vector: ndarray,
+                         parameter: Symbol, eps: float) -> float:
     function_z = lambdify(
         args=(parameter,),
         expr=func(parameterized_vector[0][0], parameterized_vector[1][0]))
 
     left, right = find_min_border(
-        f=function_z,
-        start=0.0,
-        init_step=0.25,
-        step_expand=1.1,
-        max_iters=10)
+        f=function_z, start=0.0, init_step=0.25, step_expand=1.1, max_iters=10)
 
     argmin_f = bisect_extremum(
         f=function_z, a=left, b=right, eps=eps, mode='min')
