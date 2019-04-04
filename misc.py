934565254430d2c78f5be86c313faa6ce3032864
zsxoff <zsxoff@gmail.com>
2019-04-04 19:26:14 +0400

    change func2d_to_string

--- misc.py
+++ misc.py
@@ -26,10 +26,22 @@ def inputf(text):
 
 
 def func2d_to_string(f: Callable) -> str:
+    """
+    Convert f: Callable function to latex-like string.
+
+    """
     s_x = Symbol('x')
     s_y = Symbol('y')
     f_print = lambdastr((s_x, s_y), f(s_x, s_y))
 
-    f_print = f_print.replace('lambda x,y:', '').replace('**', '^').strip()
+    replace_dict = {
+        'lambda x,y:': '',
+        '**': '^',
+        'numpy': '',
+    }
+
+    for key in replace_dict:
+        f_print = f_print.replace(key, replace_dict[key])
+    f_print = f_print.strip()
 
     return f_print[1:len(f_print) - 1]
