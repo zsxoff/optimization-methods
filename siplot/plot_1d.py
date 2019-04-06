17dad7a325a52f8eb00fd76fb82284cb3115615f
zsxoff <zsxoff@gmail.com>
2019-04-06 19:10:38 +0400

    rename graph variable

--- siplot/plot_1d.py
+++ siplot/plot_1d.py
@@ -14,19 +14,19 @@ def plot_1d(func,
 
     """
     fig = plt.figure(1)
-    graph1 = fig.add_subplot(1, 1, 1)
+    graph = fig.add_subplot(1, 1, 1)
 
     # TODO set_yticks?
 
-    graph1.set_xticks(np.arange(graph_x_min, graph_x_max, 0.5))
-    graph1.set_xticks(np.arange(graph_x_min, graph_x_max, 0.1), minor=True)
-    graph1.grid(which='minor', alpha=0.1, linestyle="-")
-    graph1.grid(which='major', alpha=0.5, linestyle="-")
+    graph.set_xticks(np.arange(graph_x_min, graph_x_max, 0.5))
+    graph.set_xticks(np.arange(graph_x_min, graph_x_max, 0.1), minor=True)
+    graph.grid(which='minor', alpha=0.1, linestyle="-")
+    graph.grid(which='major', alpha=0.5, linestyle="-")
 
     # Plot some dots.
     if dots:
         for dot in dots:
-            graph1.plot(dot.x, dot.y, dot.color)
+            graph.plot(dot.x, dot.y, dot.color)
 
             if dot.x > graph_x_max:
                 graph_x_max = dot.x
@@ -37,20 +37,20 @@ def plot_1d(func,
     # Plot some horizontal lines.
     if lines_xh:
         for line in lines_xh:
-            graph1.axhline(y=line.y,
-                           linestyle=":",
-                           linewidth=1.5,
-                           color=line.color)
+            graph.axhline(y=line.y,
+                          linestyle=":",
+                          linewidth=1.5,
+                          color=line.color)
 
             # TODO graph_ymin graph_ymax
 
     # Plot some vertical lines.
     if lines_xv:
         for line in lines_xv:
-            graph1.axvline(x=line.x,
-                           linestyle=":",
-                           linewidth=1.5,
-                           color=line.color)
+            graph.axvline(x=line.x,
+                          linestyle=":",
+                          linewidth=1.5,
+                          color=line.color)
 
             if line.x > graph_x_max:
                 graph_x_max = line.x
@@ -60,7 +60,7 @@ def plot_1d(func,
 
     # Plot function.
     X = np.arange(graph_x_min, graph_x_max, 0.01)
-    graph1.plot(X, func(X))
+    graph.plot(X, func(X))
 
     # Plot title.
     plt.title(r'${}$'.format(func1d_to_string(func)), fontsize=12)
