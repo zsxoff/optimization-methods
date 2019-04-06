70bf958cbd856a8d0133bd67d1caf97f60dcd3ed
zsxoff <zsxoff@gmail.com>
2019-04-06 19:34:32 +0400

    replace plot code with function

--- example_find_min_border.py
+++ example_find_min_border.py
@@ -1,48 +1,29 @@
 #!/usr/bin/python3
 
-import numpy as np
-import matplotlib.pyplot as plt
 from simethods.borders import find_min_border
+from siplot.plot import LineXV
+from siplot.plot_1d import plot_1d
 
 
 def f(x):
-    # TODO Doc
-    # return x**3 - 7.5 * x**2 + 15.9 * x - 7.8
+    """
+    Test function.
+
+    """
     return x ** 2 - x ** 4
 
 
 def main():
     # TODO Doc
-    # ----- Plot graph -----
-    GRAPH_L = -1.2
-    GRAPH_R = 1.2
 
     l_border, r_border = find_min_border(f)
 
-    fig = plt.figure(1)
-    graph1 = fig.add_subplot(1, 1, 1)
-    graph1.set_xticks(np.arange(GRAPH_L, GRAPH_R, 0.5))
-    graph1.set_xticks(np.arange(GRAPH_L, GRAPH_R, 0.1), minor=True)
+    lines_xv = [
+        LineXV(l_border, 'g'),
+        LineXV(r_border, 'g')
+    ]
 
-    graph1.grid(which='minor', alpha=0.1, linestyle="-")
-    graph1.grid(which='major', alpha=0.5, linestyle="-")
-
-    graph1.axvline(x=l_border, linestyle=":", linewidth=1.5, color='g')
-    graph1.axvline(x=r_border, linestyle=":", linewidth=1.5, color='g')
-
-    X = np.arange(GRAPH_L, GRAPH_R, 0.01)
-    F = f(X)
-
-    graph1.plot(X, F)
-    # graph1.plot(x_min, f_min, 'bo')
-    # graph1.plot(x_max, f_max, 'ro')
-
-    # Plot title.
-    plt.title(r'$f(x) = x^2$', fontsize=12)
-    plt.xlabel('x', fontsize=16, style="italic")
-    plt.ylabel('f(x)', fontsize=16, style="italic")
-
-    plt.show()
+    plot_1d(f, graph_x_min=-1, graph_x_max=1, lines_xv=lines_xv)
 
 
 if __name__ == '__main__':
