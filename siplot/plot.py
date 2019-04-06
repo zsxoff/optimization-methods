30d505e67451a57c798ee6b4a9fc071207c69c91
zsxoff <zsxoff@gmail.com>
2019-04-06 19:18:19 +0400

    add vector namedtuple

--- siplot/plot.py
+++ siplot/plot.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 
 Dot = namedtuple('Dot', ['x', 'y', 'color'])
+Vector = namedtuple('Vector', ['values'])
 LineXH = namedtuple('Line', ['y', 'color'])
 LineXV = namedtuple('Line', ['x', 'color'])
