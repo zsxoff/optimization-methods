041aaf9e8614332b48dc429572af74003dbef249
zsxoff <zsxoff@gmail.com>
2019-04-06 19:07:00 +0400

    add plot_1d

--- siplot/plot_1d.py
+++ siplot/plot_1d.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from misc import func1d_to_string
 
 
 def plot_1d(func,
@@ -15,6 +16,8 @@ def plot_1d(func,
     fig = plt.figure(1)
     graph1 = fig.add_subplot(1, 1, 1)
 
+    # TODO set_yticks?
+
     graph1.set_xticks(np.arange(graph_x_min, graph_x_max, 0.5))
     graph1.set_xticks(np.arange(graph_x_min, graph_x_max, 0.1), minor=True)
     graph1.grid(which='minor', alpha=0.1, linestyle="-")
@@ -25,6 +28,12 @@ def plot_1d(func,
         for dot in dots:
             graph1.plot(dot.x, dot.y, dot.color)
 
+            if dot.x > graph_x_max:
+                graph_x_max = dot.x
+
+            if dot.x < graph_x_min:
+                graph_x_min = dot.x
+
     # Plot some horizontal lines.
     if lines_xh:
         for line in lines_xh:
@@ -32,6 +41,8 @@ def plot_1d(func,
                            linestyle=":",
                            linewidth=1.5,
                            color=line.color)
+
+            # TODO graph_ymin graph_ymax
 
     # Plot some vertical lines.
     if lines_xv:
@@ -41,12 +52,18 @@ def plot_1d(func,
                            linewidth=1.5,
                            color=line.color)
 
+            if line.x > graph_x_max:
+                graph_x_max = line.x
+
+            if line.x < graph_x_min:
+                graph_x_min = line.x
+
     # Plot function.
     X = np.arange(graph_x_min, graph_x_max, 0.01)
     graph1.plot(X, func(X))
 
     # Plot title.
-    plt.title(r'$f(x) = x^3 - 7.5x^2 + 15.9x - 7.8$', fontsize=12)
+    plt.title(r'${}$'.format(func1d_to_string(func)), fontsize=12)
     plt.xlabel('x', fontsize=16, style="italic")
     plt.ylabel('f(x)', fontsize=16, style="italic")
 
